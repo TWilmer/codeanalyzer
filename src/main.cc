877227936514f255c9bee502c38d22bf97fbfba3
Thorsten Wilmer <thorsten@luna.(none)>
2010-11-05 12:13:48 +0530

    Basic stuff

--- src/main.cc
+++ src/main.cc
@@ -20,22 +20,78 @@
 #include <gtkmm.h>
 #include <iostream>
 
+#include <fcntl.h>
+#include <string.h>
+#ifdef ENABLE_NLS
+#  include <libintl.h>
+#endif
+
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+#include <list>
+
+#include <stdio.h>
+#include <sys/time.h>
+#include <time.h>
+#include <unistd.h>
+
+
+namespace icon
+{
+#include "icon.h"
+}
+;
+
+using Glib::ustring;
 
 
 /* For testing propose use the local (not installed) ui file */
 /* #define UI_FILE PACKAGE_DATA_DIR"/codeanalyzer/ui/codeanalyzer.ui" */
-#define UI_FILE "src/codeanalyzer.ui"
+
+extern "C"
+{
+#ifdef GLIBMM_WIN32
+#define GLADE_START binary_codeanalyzer_ui_start
+#define GLADE_END binary_codeanalyzer_ui_end
+#else
+#define GLADE_START _binary_codeanalyzer_ui_start
+#define GLADE_END _binary_codeanalyzer_ui_end
+#endif
+   extern char GLADE_START;
+   extern char GLADE_END;
+}
+
+struct IconData
+{
+      const guint8 *data;
+      unsigned int length;
+      int size;
+};
+
+static const IconData sIconData[] =
+   {
+      { icon::icon64, sizeof(icon::icon64), 64 },
+      { icon::icon16, sizeof(icon::icon16), 16 },
+      { icon::icon32, sizeof(icon::icon32), 32 },
+
+   };
+
    
 int
 main (int argc, char *argv[])
 {
 	Gtk::Main kit(argc, argv);
-	
+
+	int binary_gtk_pinmaster_glade_size = &GLADE_END - &GLADE_START;
+
 	//Load the Glade file and instiate its widgets:
 	Glib::RefPtr<Gtk::Builder> builder;
 	try
 	{
-		builder = Gtk::Builder::create_from_file(UI_FILE);
+		builder = Gtk::Builder::create_from_string(ustring(&GLADE_START, binary_gtk_pinmaster_glade_size));
+
 	}
 	catch (const Glib::FileError & ex)
 	{
@@ -44,6 +100,22 @@ main (int argc, char *argv[])
 	}
 	Gtk::Window* main_win = 0;
 	builder->get_widget("main_window", main_win);
+
+ Glib::RefPtr<Gtk::IconFactory> factory = Gtk::IconFactory::create();
+
+   std::list<Glib::RefPtr<Gdk::Pixbuf> > iconList;
+
+   for (int i = 0; i < sizeof(sIconData) / sizeof(sIconData[0]); i++)
+   {
+
+      const IconData &icon_data = sIconData[i];
+
+      iconList.push_back(Gdk::Pixbuf::create_from_inline(icon_data.length, icon_data.data));
+
+   }
+
+   Gtk::Window::set_default_icon_list(iconList);
+
 	if (main_win)
 	{
 		kit.run(*main_win);
